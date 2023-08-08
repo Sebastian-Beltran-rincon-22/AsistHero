7cc633691d5ab90a6fe6eab37683969e4c39d90d
sewastian <sebastianrincon834@gmail.com>
2023-08-08 18:18:48 -0500

    feat [app.module.ts]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './core/loader/loader.component';
+import { ItemComponent } from './item/item.component';
+import { PagesModule } from './pages/pages.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
