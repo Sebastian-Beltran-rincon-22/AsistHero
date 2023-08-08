b331394cf657ee961dc84ede9f8dd521decd14dd
sewastian <sebastianrincon834@gmail.com>
2023-08-08 18:18:34 -0500

    feat [app.component.ts]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeroService } from './core/service/service.service';
+import { Item } from './shared/models/item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private HeroService:HeroService){
+
+  }
+
   title = 'AsistHero';
+  public allHero: any = []
+  currentDate: Date = new Date()
+  public done: any = []
+
+  taskTitle = ""
+  addTitle(title: string):void{
+    this.taskTitle = title
+  }
+
+  ngOnInit():void{
+    this.loadData()
+  }
+
+  public loadData(){
+    this.HeroService.getTask('https://superhero-xg93.onrender.com/asisthero/')
+    .subscribe(response =>{
+      console.log(response)
+      this.allHero = response
+    })
+  }
+  addHero(title: string, img: string, id: Number){
+    const newTask = {
+      heroId: id,
+      namehero: title,
+      imgHero: img,
+      done: false
+    }
+    this.allHero.unshift(newTask)
+    console.log(newTask)
+    this.HeroService.postTask("https://superhero-xg93.onrender.com/asisthero/create",newTask)
+    .subscribe(data =>{
+      console.log(data)
+    })
+  }
+  //GerForID
+  get items(){
+    if (this.done === "true"){
+      return this.allHero.filter((item:Item)=> item.done === true)
+    }
+    return this.allHero.filter((item:Item) => item.done === false)
+  }
+
+  updateHero(title: string, img: string, id: Number):void{
+    const updateTask = {
+      heroId: id,
+      namehero: title,
+      imgHero: img,
+      done: false
+    }
+    this.HeroService.updateTask(`https://superhero-xg93.onrender.com/asisthero/update/${id}`,updateTask)
+    .subscribe(data =>{
+      console.log(data)
+    })
+  }
+  deleteHero(heroId:Number){
+    this.HeroService.deleteTask(`https://superhero-xg93.onrender.com/asisthero/delete/${heroId}`)
+    .subscribe(responde =>{
+      console.log(responde)
+    })
+  }
 }
