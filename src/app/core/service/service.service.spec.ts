3e40201bd0cb9cbb1ec78c0e5d62db9e0bba6676
sewastian <sebastianrincon834@gmail.com>
2023-08-08 18:41:41 -0500

    update [services -> services]

--- src/app/core/service/service.service.spec.ts
+++ src/app/core/service/service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceService } from './service.service';
+import { HeroService } from './service.service';
 
 describe('ServiceService', () => {
-  let service: ServiceService;
+  let service: HeroService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ServiceService);
+    service = TestBed.inject(HeroService);
   });
 
   it('should be created', () => {
