af9c44ae0dc9c58617c6167189506648af4746fc
sewastian <sebastianrincon834@gmail.com>
2023-08-09 11:42:34 -0500

    update [home -> home.component.ts]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeroService } from 'src/app/core/service/service.service';
+import { Item } from 'src/app/shared/models/item';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private HeroService: HeroService){
 
+  }
+  @Input() Item!: AppComponent
+
+
+  
 }
+
+
+
