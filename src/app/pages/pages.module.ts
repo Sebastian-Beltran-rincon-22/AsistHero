b8ace52ec8f10f214c7af3703e64df872c178181
sewastian <sebastianrincon834@gmail.com>
2023-08-09 11:40:07 -0500

    udpate [pages -> pages.module.ts]

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
