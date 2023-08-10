98462f5fe6b49d404e52b137c9e828e9e4d41826
sewastian <sebastianrincon834@gmail.com>
2023-08-09 19:21:31 -0500

    update [home -> home.component.ts]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { HeroService } from 'src/app/core/service/service.service';
 import { Item } from 'src/app/shared/models/item';
-import { AppComponent } from 'src/app/app.component';
+import { HeroService } from 'src/app/core/service/service.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +8,73 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private HeroService: HeroService){
+  constructor(private HeroService: HeroService){}
 
+  title = 'AsistHero';
+  public allHero: any = []
+  currentDate: Date = new Date()
+  public done: any = []
+
+  taskTitle = ""
+  addTitle(title: string):void{
+    this.taskTitle = title
   }
-  @Input() Item!: AppComponent
+
+  ngOnInit():void{
+    this.loadData()
+  }
+
+  public loadData(){
+    this.HeroService.getTask('https://superhero-xg93.onrender.com/asisthero/')
+    .subscribe(response =>{
+      console.log(response)
+      this.allHero = response
+    })
+  }
+
+  onNewHeroCreated(newHero: any) {
+    this.allHero.unshift(newHero); // Agregar el nuevo héroe al array
+  }
+
+  //GerForID
+  // get items(){
+  //   if (this.done === "true"){
+  //     return this.allHero.filter((item:Item)=> item.done === true)
+  //   }
+  //   return this.allHero.filter((item:Item) => item.done === false)
+  // }
+
+  updateHero(name: string, image: string, id: number):void{
+    const updateHero = {
+      heroId: id,
+      namehero: name,
+      imgHero: image,
+      done: false
+    }
+    this.HeroService.updateTask(`https://superhero-xg93.onrender.com/asisthero/update/${id}`,updateHero)
+    .subscribe(data =>{
+      console.log(data)
+      this.loadData()
+    })
+  }
+  deleteHero(item: any){
+    this.allHero = this.allHero.filter((hero: Item) => hero.heroId !== item.heroId);
+    this.HeroService.deleteTask(`https://superhero-xg93.onrender.com/asisthero/delete/${item.heroId}`)
+    .subscribe(responde =>{
+      console.log(responde)
+    })
+  }
 
 
-  
+  toggleItemDone(item: any) {
+    item.done = !item.done; // Cambia el estado contrario y envia la peticion al back para actualizar
+    console.log(this.allHero)
+    this.HeroService.updateTask(`https://superhero-xg93.onrender.com/asisthero/update/${item.heroId}`, item)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
 }
 
 
